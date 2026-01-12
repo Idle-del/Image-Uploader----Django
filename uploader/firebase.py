029d45cbe7a831d93b6a78ae7862afc691c4a183
Idle del <magarkiran436@gmail.com>
2026-01-12 17:24:07 +0545

    Update firebase path for PythonAnywhere

--- uploader/firebase.py
+++ uploader/firebase.py
@@ -2,5 +2,5 @@ import firebase_admin
 from firebase_admin import credentials
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(r'C:\Users\KIRAN\OneDrive\Documents\firebase-service-account.json')
+    cred = credentials.Certificate('/home/Kiran117/Image-Uploader----Django/firebase-service-account.json')
     firebase_admin.initialize_app(cred)
